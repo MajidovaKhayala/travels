c2c8b190eda91a4ceb69dd27826b0035ae2ff6a1
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-08-05 10:58:30 +0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import ToursPage from "./pages/ToursPage";
+import VisaPage from "./pages/VisaPage";
+import SanatoriumsPage from "./pages/SanatoriumsPage";
+import RequestPage from "./pages/RequestPage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/tours" element={<ToursPage />} />
+            <Route path="/visa" element={<VisaPage />} />
+            <Route path="/sanatoriums" element={<SanatoriumsPage />} />
+            <Route path="/request" element={<RequestPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
